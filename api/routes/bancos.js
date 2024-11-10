db89d606ba0b02172bd996171b8957bb2de16502
Gerardo Castro <104610703+LittleWea@users.noreply.github.com>
2024-11-09 20:12:02 -0600

    Update bancos.js

--- api/routes/bancos.js
+++ api/routes/bancos.js
@@ -1,46 +1,54 @@
-// routes/users.js
+//Librerias a usar en el modulo
 const express = require('express');
 const router = express.Router();
 const connection = require('../resources/db');
 
-// Ruta para crear un usuario
+// Ruta para obtener bancos segun un termino
 router.post('/obtener', (req, res) => {
-  console.log(req.session.user)
+  //Verificar inicio de sesion
   if (req.session.user) {
+    //Listar bancos
     const query = "SELECT * FROM Bancos WHERE Nombre LIKE CONCAT('%', ?, '%')";
     connection.query(query, [req.body.Buscar], (err, results) => {
       if (err) {
+        //Enviar error
         return res.status(201).json({ mensaje: 'Error en la búsqueda', error: err });
       }
+      //Enviar respuesta
       return res.status(200).json({ mensaje: 'Datos obtenidos exitosamente', data: results });
     });
   } else {
+    //Enviar error
     return res.status(201).json({ mensaje: 'No hay sesión activa' });
   }
 });
 
-// Ruta para crear un usuario
+// Ruta para obtener banco
 router.post('/obtenerPorId', (req, res) => {
   // Obtener los datos del cuerpo de la solicitud
-
   const { ID } = req.body;
+  //Verificar inicio de sesion
   if (req.session.user) {
+    //Obtener banco
     const query = "SELECT * FROM Bancos WHERE ID = ?";
     connection.query(query, [ID], (err, results) => {
       if (err) {
+        //Enviar error
         return res.status(201).json({ mensaje: 'Error al crear el usuario', error: err });
       }
-
+      //Enviar respuesta
       res.status(201).json({
         mensaje: 'Información obtenida',
         data: results
       });
     });
   } else {
+    //Enviar error
     return res.status(201).json({ mensaje: 'No session', error: { Error: true } });
   }
 });
 
+//Ruta para verificar inicio de sesion
 router.post('/getSession', async (req, res) => {
   if (req.session.user !== undefined) {
     return res.status(201).json({
